95467cc1dd95b4e5c6df5edb52b35d20ad681e89
Dogukan <cirpan.dogukan5959@gmail.com>
2020-02-06 14:22:45 +0100

    add Account validation by email System

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -16,9 +18,10 @@ class RegistrationController extends AbstractController
      * @Route("/register", name="app_register")
      * @param Request $request
      * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param \Swift_Mailer $mailer
      * @return Response
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(Request $request, \Swift_Mailer $mailer,UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -33,17 +36,58 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            // generate uuid for validation token
+            $regToken = uuid_create((UUID_TYPE_RANDOM));
+            $user->setRegToken($regToken);
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('home');
+            $message = (new \Swift_Message('Account confirmation'))
+                ->setFrom(['cirpan.dogukan5959@gmail.com' => 'SnowTricks'])
+                ->setTo($user->getEmail())
+                ->setSubject('Register confirmation link')
+                ->setBody($this->renderView('email/register.html.twig', ['token' => $regToken]), 'text/html');
+            $mailer->send($message);
+            $this->addFlash('info', 'Check your email and click on the confirmation link, thank you.');
+
+            return $this->redirectToRoute('app_home');
+
         }
+
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/register/{reg_token}", name="app_register_validation")
+     * @param Request $request
+     * @return Response
+     */
+    public function registerValidation(Request $request, $reg_token): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $entityManager->getRepository(User::class)->findOneBy(
+            [
+                'reg_token' => $reg_token
+            ]
+        );
+
+        $error = false;
+
+        if ($user) {
+            $user->setRegToken(null);
+            $entityManager->persist($user);
+            $entityManager->flush();
+        } else {
+            $error = true;
+        }
+
+        return $this->render('registration/validation.html.twig', ['error' => $error]);
     }
 }
