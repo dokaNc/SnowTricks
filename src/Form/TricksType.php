98dc169869cadae257c15b5d66f6b3430a3d5d0e
Dogukan <cirpan.dogukan5959@gmail.com>
2020-04-05 13:25:13 +0200

    add ChoiceType class for Category

--- src/Form/TricksType.php
+++ src/Form/TricksType.php
@@ -4,7 +4,7 @@ namespace App\Form;
 
 use App\Entity\Tricks;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -27,7 +27,14 @@ class TricksType extends AbstractType
                     'placeholder' => 'Content'
                 ]
             ])
-            ->add('category')
+            ->add('category', ChoiceType::class, [
+                'choices' => array(
+                    'Grabs' => 'Grabs',
+                    'Rotations' => 'Rotations',
+                    'Flips' => 'Flips',
+                    'Slides' => 'Slides'
+                )
+            ])
         ;
     }
 
